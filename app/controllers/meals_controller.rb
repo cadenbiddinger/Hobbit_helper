c22b4c3bf8cf7c49d353862bbb10a9c5b10e66b2
Andre Leveille <andre.joe.leveille@gmail.com>
2020-06-06 11:13:59 -0600

    added models/controllers

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -1,13 +1,49 @@
 class MealsController < ApplicationController
+  before_action :set_meal, only: [:show, :update, :edit, :destroy]
+  
   def index
+    @meals = current_user.meals
   end
 
   def show
   end
 
   def new
+    @meal = Meal.new
+  end
+
+  def create
+    @meal = current_user.meals.new(meal_params)
+    if @meal.save
+      redirect_to meals_path
+    else 
+      render :new
+    end
   end
 
   def edit
   end
+
+  def update
+    if @meal.update(meal_params)
+      redirect_to meals_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy 
+    @meal.destroy
+    # current_user.meals.find(params[:id]).destroy
+    redirect_to meals_path
+  end
+
+  private 
+    def meal_params
+      params.requre(:meal).permit(:breakfast, :second_breakfast, :elevensies, :luncheon, :afternoon_tea, :dinner, :supper)
+    end
+
+    def set_meal
+      @meal = Meal.find(params[:id])
+    end
 end
